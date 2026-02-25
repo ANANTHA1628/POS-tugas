eb0f4afd9f61cf81fea055a05528d44de221a420
ANANTHA1628 <aryaputraferdiananta@gmail.com>
2026-02-26 05:23:04 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,8 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
 
-// 1. Halaman Home
 Route::get('/', [HomeController::class, 'index']);
 
-// 2. Halaman Products (Route Prefix)
 Route::prefix('category')->group(function () {
     Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
     Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
@@ -17,8 +15,6 @@ Route::prefix('category')->group(function () {
     Route::get('/baby-kid', [ProductController::class, 'babyKid']);
 });
 
-// 3. Halaman User (Route Param)
 Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
 
-// 4. Halaman Penjualan
 Route::get('/sales', [SalesController::class, 'index']);
